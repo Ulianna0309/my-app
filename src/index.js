81ae3764c5da732d60bd3183862ecb998e9f092e
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-19 15:29:26 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello World!</h2>;
+const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
