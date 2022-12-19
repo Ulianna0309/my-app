a4d37193c38fe74a5c797352a46449e0b7c20b25
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-19 15:56:01 +0200

    122

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import App from './App';
 
 const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world');
 
+const text = 'Hi, Julia';
+
+const elements = (
+  <div>
+      <h2 className="title">Teкст: {text}</h2>
+      <input type='text'/>
+      <label htmlFor="btn"></label>
+      <button id="btn" tabIndex={0}>cool</button>
+  </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  elem
+  elements
 );
