0b60f59764580aafc1f13e50bf10ef35aac3262c
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-20 20:06:50 +0200

    130

--- src/App.js
+++ src/App.js
@@ -36,14 +36,32 @@ function Btn() {
   return <button>{logged ? 'Enter': text}</button>
 }
 
-function WhoAmI ({name, surname, link}){
-  return(
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
-}
+class WhoAmI extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++'
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render(){
+      const{name, surname, link} = this.props;
+      return(
+        <div>
+          <button onClick={this.nextYear}>{this.state.text}</button>
+          <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+          <a href={link}>My profile</a>
+        </div>
+      )
+    }
+  }
 
 
 function App() {
@@ -56,8 +74,8 @@ function App() {
         <Btn/>
         <Field2/>
 
-        <WhoAmI name={() => {return 'Jon'}} surname="Smoth" link="gmail.com"/>
-        <WhoAmI name={() => {return 'Alex'}} surname="Smith" link="gmail.ua"/>
+        <WhoAmI name='Jon' surname="Smoth" link="gmail.com"/>
+        <WhoAmI name='Alex' surname="Smith" link="gmail.ua"/>
     </div>
   );
 }
