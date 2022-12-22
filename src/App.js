b3d4407075ef4528d236aff476a8460e23ccc687
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-22 16:44:59 +0200

    add fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, StrictMode} from 'react';
+import {Component, StrictMode, Fragment} from 'react';
 import './App.css';
 
 const Header = () => {
@@ -66,7 +66,7 @@ class WhoAmI extends Component {
       const{name, surname, link} = this.props;
       const {position, years} = this.state;
       return(
-        <div>
+        <>
           <button onClick={this.nextYear}>{this.state.text}</button>
           <h1>My name is {name}, surname - {surname}, 
           age - {years}, 
@@ -76,7 +76,7 @@ class WhoAmI extends Component {
             <span>Введите должность</span>
             <input type='text' onChange={(e) => this.commitInputChanges(e, 'some color')} />
           </form>
-        </div>
+          </>
       )
     }
   }
