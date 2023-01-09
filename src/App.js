d052a6b323ec4bbdc3923dee05dd40723da380c9
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2023-01-09 12:19:05 +0200

    props.children

--- src/App.js
+++ src/App.js
@@ -131,12 +131,21 @@ export const Container = styled.div`
   background: rgb(131,58,180);
   background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
   `
-  
 
+const Greating = (props) => {
+  return(
+    <div className={'mb-3 p-3 border border-' + props.color}>
+        {props.children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <Wrapper>
+      <Greating color={'primary'}>
+         <h2>jhjhjhjh</h2>
+      </Greating>
       <StrictMode>
         <Header/>
       </StrictMode>
