ddee7174e5cf0ef3b7db04ac2b3a78924477d4e4
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2023-01-12 15:53:26 +0200

    Portal

--- src/NewForm.js
+++ src/NewForm.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {Container} from 'react-bootstrap';
 
 class NewForm extends Component {
+    state = {
+        advOpen: true
+    }
 
+    componentDidMount(){
+        setTimeout(this.handleClick, 3000)
+    }
+
+    handleClick = () => {
+       this.setState(({advOpen}) => ({
+          advOpen: !advOpen
+       }))
+    }
+
+    
     render() {
         return (
             <Container>
-                <form className="w-50 border mt-5 p-3 m-auto" 
+                <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
                 style={{'overflow': 'hidden', 
                         'position': 'relative'}}>
                     <div className="mb-3">
@@ -17,19 +32,37 @@ class NewForm extends Component {
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
-                    <div 
-                    style={{'width': '500px', 
-                            'height': '150px', 
-                            'backgroundColor': 'red', 
-                            'position': 'absolute', 
-                            'right': '-50%', 
-                            'bottom': '-50%'}}>
-                        Hello
-                    </div>
+                    {
+                        this.state.advOpen ?
+                            <Portal>
+                                <Msg/>
+                            </Portal> : null
+                    }
+                   
                 </form>
             </Container>
         )
     }
+}
+
+const Portal = (props) => {
+    const node = document.createElement('section');
+    document.body.appendChild(node);
+    return ReactDOM.createPortal(props.children, node);
+}
+
+const Msg = () =>{
+    return(
+        <div 
+            style={{'width': '300px', 
+                'height': '150px', 
+                'backgroundColor': 'red', 
+                'position': 'absolute', 
+                'right': '0', 
+                'bottom': '0'}}>
+            Hello
+        </div>
+    )
 }
 
 export default NewForm;
